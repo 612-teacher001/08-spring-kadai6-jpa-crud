b7a3440234e460e84b271f9edf225a2024481332
villageworks <villageworks@gmail.com>
2025-04-01 23:54:11 +0900

    ver.0.20: Implementing the registration process

--- src/main/java/com/example/demo/controller/ItemController.java
+++ src/main/java/com/example/demo/controller/ItemController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.entity.Item;
 import com.example.demo.repository.ItemRepository;
@@ -15,6 +17,30 @@ public class ItemController {
 	
 	@Autowired
 	ItemRepository itemRepository;
+	
+	@PostMapping("/items/add")
+	public String store(@RequestParam(defaultValue = "0") Integer categoryId,
+						@RequestParam String name,
+						@RequestParam(defaultValue = "0") Integer price) {
+		// リクエエストパラメータをもとに商品クラスをインスタンス化
+		Item item = new Item(categoryId, name, price);
+		
+		// 商品インスタンスをitemsテーブルに登録
+		itemRepository.save(item);
+		
+		// 画面遷移
+		return "redirect:/items";
+	}
+	
+	/**
+	 * 商品登録画面表示
+	 * @return 商品登録画面のThymeleafテンプレート名
+	 */
+	@GetMapping("/items/add")
+	public String create() {
+		// 画面遷移
+		return "addItem";
+	}
 	
 	/**
 	 * 商品一覧画面表示
